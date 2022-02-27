7fd8c904383ffde987e06b125399d13fe488947d
IvorWithCrossbow <sergey.shin.t@gmail.com>
2022-02-28 00:17:28 +0600

    1 final

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,14 +1,20 @@
-let timer = document.getElementById("timer")
+let timer = document.getElementById("timer");
+timer.textContent = "00:00:59";
+let final;
 
 let interval = setInterval(() => {
-    let date = new Date(0);
-    date.setSeconds(timer.textContent);
-    let timeString = date.toISOString().substr(11, 6) + (timer.textContent -= 1);
-    console.log(timeString);
-    console.log(timer.textContent);
+    // console.log(timer.textContent);
+    let arr = timer.textContent.split(":");
+    // console.log(arr);
+    let parsed = parseInt(arr[2], 10) - 1;
+    let string = parsed.toString();
+    // console.log(string);
+    arr.splice(2, 1, string);
+    final = arr.join(":");
+    // console.log(final);
+    timer.textContent = final;
     
-
-    if (timer.textContent === "0") {
+    if (timer.textContent === "00:00:00") {
         alert("Вы победили в конкурсе!");
         clearInterval();
         location.assign("http://www.mozilla.org");
