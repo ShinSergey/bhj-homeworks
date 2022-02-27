1cccf90e71572738632eb87287044aee9f823279
IvorWithCrossbow <sergey.shin.t@gmail.com>
2022-02-28 00:17:02 +0600

    1 final

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,10 +4,17 @@ const speed = document.getElementById("clicker__speed");
 
 let flag = 1;
 let count = 0;
-let startTime = 0;
+let startTime = new Date();
+console.log(startTime);
 
 cookie.onclick = () => {
-    start = new Date();
+    let newTime = new Date();
+    console.log(newTime);
+    let diff = newTime - startTime;
+    console.log(diff);
+    startTime = newTime;
+    let clickSpeed = 1000 / diff;
+    speed.textContent = clickSpeed.toFixed(2);
     count += 1;
     cookie.width = count % 2 ? 250 : 200;
     counter.textContent = count;
