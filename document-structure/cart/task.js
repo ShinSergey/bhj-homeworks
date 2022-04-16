92433b8d0b318746d8025593c9ceb296d814098f
Sergey <sergey.shin.t@gmail.com>
2022-04-16 21:01:10 +0600

    3

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -21,8 +21,8 @@ function inc(element) {
 
 function dec(element) {
     element.nextSibling.nextSibling.textContent--;
-    if (element.nextSibling.nextSibling.textContent < 0) {
-        element.nextSibling.nextSibling.textContent = 0;
+    if (element.nextSibling.nextSibling.textContent < 1) {
+        element.nextSibling.nextSibling.textContent = 1;
     }
 }
 
@@ -30,39 +30,51 @@ function add(element) {
     let img = element.closest(".product__controls").previousSibling.previousSibling;
     let quantity = element.previousSibling.previousSibling.querySelector(".product__quantity-value").textContent;
     element.previousSibling.previousSibling.querySelector(".product__quantity-value").textContent = 1;
-    let addedItems = cart.getElementsByClassName("cart__product-image");
-    if (addedItems.length === 0) {
-        cart.insertAdjacentHTML("beforeend", `
-    <div class="cart__product" data-id="">
-                <img class="cart__product-image" src="">
-                <div class="cart__product-count"></div>
-            </div>`);
-        let newIn = cart.lastChild;
-        newIn.setAttribute("data-id", cart.getElementsByClassName("cart__product").length);
-        newIn.firstChild.nextSibling.src = img.currentSrc;
-        newIn.lastChild.previousSibling.textContent = quantity;
-    } else {
-        let addedItem;
-        for (let i = 0; i < addedItems.length; i++) {
-            if (addedItems[i].src === img.currentSrc) {
-                addedItem = addedItems[i];
-            }
-        }
+    let addedItems = Array.from(cart.getElementsByClassName("cart__product-image"));
+
+    let addedItem = addedItems.find(function (element, index, array) {
+        element === img;
+    })
         if (addedItem) {
             let number1 = Number(addedItem.nextSibling.nextSibling.textContent);
             let number2 = Number(quantity);
             addedItem.nextSibling.nextSibling.textContent = number1 + number2;
-        } else {
-            cart.insertAdjacentHTML("beforeend", `
-    <div class="cart__product" data-id="">
-                <img class="cart__product-image" src="">
-                <div class="cart__product-count"></div>
-            </div>`);
+        }
+        
+
+
+// if () {
+        // cart.insertAdjacentHTML("beforeend", `
+    // <div class="cart__product" data-id="">
+                // <img class="cart__product-image" src="">
+                // <div class="cart__product-count"></div>
+            // </div>`);
+        // let newIn = cart.lastChild;
+        // newIn.setAttribute("data-id", cart.getElementsByClassName("cart__product").length);
+        // newIn.firstChild.nextSibling.src = img.currentSrc;
+        // newIn.lastChild.previousSibling.textContent = quantity;
+    // } else {
+        // let addedItem;
+        // for (let i = 0; i < addedItems.length; i++) {
+            // if (addedItems[i].src === img.currentSrc) {
+                // addedItem = addedItems[i];
+            // }
+        // }
+        // if (addedItem) {
+            // let number1 = Number(addedItem.nextSibling.nextSibling.textContent);
+            // let number2 = Number(quantity);
+            // addedItem.nextSibling.nextSibling.textContent = number1 + number2;
+        // } else {
+            // cart.insertAdjacentHTML("beforeend", `
+    // <div class="cart__product" data-id="">
+                // <img class="cart__product-image" src="">
+                // <div class="cart__product-count"></div>
+            // </div>`);
             let newIn = cart.lastChild;
             newIn.setAttribute("data-id", cart.getElementsByClassName("cart__product").length);
             newIn.firstChild.nextSibling.src = img.currentSrc;
             newIn.lastChild.previousSibling.textContent = quantity;
-        }
+        // }
     }
     deleteButtons = document.getElementsByClassName("cart__product-count");
     for (let d = 0; d < deleteButtons.length; d++) {
@@ -73,11 +85,7 @@ function add(element) {
             }
         }
     }
-    if (cart.firstChild) {
-        cartTitle.hidden = false;
-    } else {
-        cartTitle.hidden = true;
-    }
+    cartTitle.hidden = !cart.firstChild;
 }
 
 
--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,24 +1,35 @@
 const checkboxes = document.getElementsByClassName("interest__check");
 
 function checker(e) {
+    
     if (e.closest("label").nextElementSibling) {
         let childboxes = e.closest("label").nextElementSibling.children;
-        let parentbox;
+        let boxes = Array.from(childboxes);
+        boxes.forEach(element => {
+            let childbox = element.querySelector(".interest__check");
+            childbox.checked = !childbox.checked;
+            checker(childbox);
+        });
+
+        // for (let c = 0; c < childboxes.length; c++) {
+        //     childbox = childboxes[c].querySelector(".interest__check");
+        //     childbox.checked = !childbox.checked;
+        //     if (childbox) {
+        //         checker(childbox);
+        //     }
+        // }
+        // checker(childbox);
+    }
+    let parentbox;
         if (e.closest("ul").previousElementSibling.children) {
             parentbox = e.closest("ul").previousElementSibling.children;
         }
-        for (let c = 0; c < childboxes.length; c++) {
-            let childbox = childboxes[c].querySelector(".interest__check");
-            childbox.checked = !childbox.checked;
-            parentbox[0].checked = childbox.checked;
-            checker(childbox);
-        }
-    }
+    parentbox[0].checked = !parentbox[0].checked;
 }
 
 for (let i = 0; i < checkboxes.length; i++) {
     checkboxes[i].addEventListener("change", () => {
-        let checkbox = checkboxes[i]
+        let checkbox = checkboxes[i];
         checker(checkbox);
     })
 }
