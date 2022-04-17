b6d45ece7f1cbcb0e864835495f7f82d7a03f9b3
IvorWithCrossbow <sergey.shin.t@gmail.com>
2022-04-17 12:13:29 +0600

    4

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,7 +1,6 @@
 const checkboxes = document.getElementsByClassName("interest__check");
 
 function checker(e) {
-    
     if (e.closest("label").nextElementSibling) {
         let childboxes = e.closest("label").nextElementSibling.children;
         let boxes = Array.from(childboxes);
@@ -10,26 +9,22 @@ function checker(e) {
             childbox.checked = !childbox.checked;
             checker(childbox);
         });
-
-        // for (let c = 0; c < childboxes.length; c++) {
-        //     childbox = childboxes[c].querySelector(".interest__check");
-        //     childbox.checked = !childbox.checked;
-        //     if (childbox) {
-        //         checker(childbox);
-        //     }
-        // }
-        // checker(childbox);
     }
-    let parentbox;
-        if (e.closest("ul").previousElementSibling.children) {
-            parentbox = e.closest("ul").previousElementSibling.children;
-        }
-    parentbox[0].checked = !parentbox[0].checked;
+}
+
+function checkerUp(e) {
+    if (e.closest("ul").previousElementSibling.children) {
+        let parentbox;
+        parentbox = e.closest("ul").previousElementSibling.children;
+        parentbox[0].checked = !parentbox[0].checked;
+        checkerUp(parentbox[0]);
+    }
 }
 
 for (let i = 0; i < checkboxes.length; i++) {
     checkboxes[i].addEventListener("change", () => {
         let checkbox = checkboxes[i];
         checker(checkbox);
+        checkerUp(checkbox);
     })
 }
