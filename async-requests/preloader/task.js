d98ed4579852a95ec67933d0bfd4afe77edab8f6
IvorWithCrossbow <sergey.shin.t@gmail.com>
2022-04-22 22:38:36 +0600

    22.04

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,47 +1,60 @@
 const items = document.getElementById("items");
 const loader = document.querySelector(".loader");
-// localStorage.clear();
 const getCurrency = new XMLHttpRequest();
 getCurrency.open("GET", "https://netology-slow-rest.herokuapp.com");
 let currency;
-let getter = localStorage.getItem("currency");
-let parsed = JSON.parse(getter);
+let get = localStorage.getItem("currency");
+let parsed = JSON.parse(get);
+function getHTML(arr) {
+  arr.forEach((element) => {
+    items.insertAdjacentHTML("afterBegin",
+      `<div class="item">
+        <div class="item__code">
+        ${element[0]}
+        </div>
+        <div class="item__value">
+        ${element[1]}
+        </div>
+        <div class="item__currency">
+          руб.
+        </div>
+      </div>`);
+  })
+}
 if (parsed) {
-    parsed.forEach((element) => {
-        items.insertAdjacentHTML("afterBegin", element);
-    })
+  getHTML(parsed)
+  loader.classList.remove("loader_active");
 }
 
 getCurrency.addEventListener("readystatechange", () => {
-    if (getCurrency.readyState === getCurrency.DONE) {
-        let parsed = JSON.parse(getCurrency.responseText);
-        console.log(parsed);
-        let currencies = parsed.response.Valute;
-        Object.entries(currencies).forEach(item => {
-            items.insertAdjacentHTML("afterbegin",
+  if (getCurrency.readyState === getCurrency.DONE) {
+    if(items.innerHTML !== "") {
+      items.innerHTML = "";
+    }
+    let parsed = JSON.parse(getCurrency.responseText);
+    let currencies = parsed.response.Valute;
+    Object.entries(currencies).forEach(item => {
+      items.insertAdjacentHTML("afterBegin",
         `<div class="item">
           <div class="item__code">
+          ${item[0]}
           </div>
-
           <div class="item__value">
-            
+          ${item[1].Value}
           </div>
           <div class="item__currency">
             руб.
           </div>
         </div>`);
-        items.firstChild.firstChild.nextSibling.textContent = item[0];
-        items.firstChild.firstChild.nextSibling.nextSibling.nextSibling.textContent = item[1].Value;
-        loader.classList.remove("loader_active");
-          }) 
-          let arr = Array.from(items.getElementsByClassName("item"));
-          currency = [];
-          localStorage.clear();
-          arr.forEach(element => {
-            arr.push(element.outerHTML);
-              localStorage.setItem("currency", JSON.stringify(arr));
-          });
-    }
+      loader.classList.remove("loader_active");
+    })
+    let arr = Array.from(items.getElementsByClassName("item"));
+    currency = [];
+    arr.forEach(element => {
+      currency.push([element.firstChild.nextSibling.textContent, element.firstChild.nextSibling.nextSibling.nextSibling.textContent]);
+    });
+    localStorage.setItem("currency", JSON.stringify(currency));
+  }
 })
 
 getCurrency.send();
